0b3e628a76de89e1537d187d26ae798cc6ab1fff
bhaskargram <bhaskarjyotisaikia078@gmail.com>
2024-07-16 01:42:07 +0530

    Add Year option in JS

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ $('#countdown').countdown({
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+	    .append("<div>" + this.leadingZeros(data.years, 2) + "<span>years</span></div>")
 	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
 	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
 	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
